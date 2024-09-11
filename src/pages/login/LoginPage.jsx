2994d4e9f51fda67faa8f0c39ac26770c73802d0
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-09-11 22:08:15 +0700

    [FEAT]: 404 page

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -6,7 +6,7 @@ export const LoginPage = () => {
     <div className="bg-white">
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="flex flex-col overflow-hidden bg-white border rounded-xl shadow-md lg:flex-row sm:mx-auto">
-          <div className="relative lg:w-1/2 bg-yellow-100">
+          <div className="relative lg:w-1/2 bg-gradient-to-br from-blue-300 to-purple-300">
             <img
               src={illust}
               alt=""
