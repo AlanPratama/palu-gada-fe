7b18986b542c4ef88584973cfdd86d51bb809cca
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-09-11 21:56:54 +0700

    [FEAT]: login page

---
+++ src/pages/login/LoginPage.jsx
@@ -0,0 +1,30 @@
+import LoginCard from "./components/LoginCard";
+import illust from "/login_illust.png";
+
+export const LoginPage = () => {
+  return (
+    <div className="bg-white">
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="flex flex-col overflow-hidden bg-white border rounded-xl shadow-md lg:flex-row sm:mx-auto">
+          <div className="relative lg:w-1/2 bg-yellow-100">
+            <img
+              src={illust}
+              alt=""
+              className="object-cover lg:absolute h-80 lg:h-full"
+            />
+            <svg
+              className="absolute top-0 right-0 hidden h-full text-white lg:inline-block"
+              viewBox="0 0 20 104"
+              fill="currentColor"
+            >
+              <polygon points="17.3036738 5.68434189e-14 20 5.68434189e-14 20 104 0.824555778 104" />
+            </svg>
+          </div>
+          <div className="flex flex-col justify-center p-8 bg-white lg:p-16 lg:pl-10 lg:w-1/2">
+            <LoginCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/login/components/LoginCard.jsx
@@ -0,0 +1,81 @@
+import { Button, Input } from "@nextui-org/react";
+import { useState } from "react";
+
+export default function LoginCard() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("LOGIN:", { email, password });
+  };
+
+  return (
+    <div className="w-full max-w-md px-8">
+      <h1 className="text-red-500 font-bold top-0 right-0 mb-10">
+        PaluGada Admin
+      </h1>
+      <div className="space-y-1 flex flex-col mb-4">
+        <h1 className="text-2xl font-bold">Welcome Back! 👋</h1>
+        <p className="font-light text-gray-500">
+          Enter your email and password to access your account
+        </p>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Input
+                id="email"
+                type="email"
+                variant="underlined"
+                placeholder="admin@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <div className="relative">
+                <Input
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  variant="underlined"
+                  placeholder="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <Button
+                  isIconOnly
+                  type="button"
+                  variant="light"
+                  className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <ion-icon name="eye-outline" className="h-4 w-4" />
+                  ) : (
+                    <ion-icon name="eye-off-outline" className="h-4 w-4" />
+                  )}
+                </Button>
+              </div>
+            </div>
+          </div>
+          <Button className="w-full my-6" color="primary" type="submit">
+            Sign in
+          </Button>
+        </form>
+      </div>
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <a
+          href="#"
+          className="text-sm text-primary underline-offset-4 transition-colors hover:underline"
+        >
+          Forgot password?
+        </a>
+      </div>
+    </div>
+  );
+}
