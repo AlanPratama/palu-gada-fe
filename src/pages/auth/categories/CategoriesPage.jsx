86fb1291bf623a68f92d7f293068aeadbe5e37ba
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-09-14 17:58:12 +0700

    [FEAT??]: date formatter in categories table

--- src/pages/auth/categories/CategoriesPage.jsx
+++ src/pages/auth/categories/CategoriesPage.jsx
@@ -86,6 +86,25 @@ const CategoriesPage = () => {
     setPage(1);
   }, []);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+
+    const dayName = new Intl.DateTimeFormat("id-ID", {
+      weekday: "long",
+    }).format(date);
+    const day = date.getDate();
+    const monthName = new Intl.DateTimeFormat("en-GB", {
+      month: "short",
+    }).format(date);
+    const year = date.getFullYear();
+    const time = date.toLocaleTimeString("en-GB", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    return `${dayName}, ${day} ${monthName} ${year} (${time})`;
+  };
+
   useEffect(() => {
     fetchCategories();
   }, [fetchCategories]);
@@ -157,8 +176,8 @@ const CategoriesPage = () => {
                 <TableRow key={category.id}>
                   <TableCell>{category.id}</TableCell>
                   <TableCell>{category.name}</TableCell>
-                  <TableCell>{category.createdAt}</TableCell>
-                  <TableCell>{category.updatedAt}</TableCell>
+                  <TableCell>{formatDate(category.createdAt)}</TableCell>
+                  <TableCell>{formatDate(category.updatedAt)}</TableCell>
                   <TableCell>
                     <Popover placement="bottom">
                       <PopoverTrigger>
